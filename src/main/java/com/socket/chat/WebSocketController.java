da20b712daeeb44664fc981ea2aef8f45e225d05
KimDoubleB <tree9295@gmail.com>
2021-07-07 14:50:09 +0900

    initial commit: plane websocket

---
+++ src/main/java/com/socket/chat/WebSocketController.java
@@ -0,0 +1,14 @@
+package com.socket.chat;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebSocketController {
+
+    @RequestMapping("/websocket")
+    public String getWebSocket() {
+        return "ws-broadcast";
+    }
+
+}
